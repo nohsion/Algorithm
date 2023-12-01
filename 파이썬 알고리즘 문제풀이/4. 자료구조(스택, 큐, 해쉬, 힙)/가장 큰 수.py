50bb2df224a662131b42b157c307fa4e2e933c78
nohsion <shtldhs123@naver.com>
2023-12-01 22:30:19 +0900

    가장 큰 수(스택) - 모범 풀이

--- 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/가장 큰 수.py
+++ 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/가장 큰 수.py
@@ -1,21 +1,33 @@
 n, m = map(int, input().split())
 
 # 내 답안
-a = [int(i) for i in str(n)]
+# a = [int(i) for i in str(n)]
+# stack = []
+# for x in a:
+#     if not stack or x <= stack[-1] or m == 0:
+#         stack.append(x)
+#         continue
+#     while x > stack[-1]:
+#         stack.pop()
+#         m -= 1
+#         if not stack or m == 0:
+#             break
+#     stack.append(x)
+#
+# for i in range(m):
+#     stack.pop()
+# for s in stack:
+#     print(s, end='')
+
+
+# 모범 풀이
+num = list(map(int, str(n)))
 stack = []
-for x in a:
-    if not stack or x <= stack[-1] or m == 0:
-        stack.append(x)
-        continue
-    while x > stack[-1]:
+for x in num:
+    while stack and x > stack[-1] and m > 0:
         stack.pop()
         m -= 1
-        if not stack or m == 0:
-            break
     stack.append(x)
-
-for i in range(m):
-    stack.pop()
-for s in stack:
-    print(s, end='')
-
+if m >이 0:
+    stack = stack[:-m]
+print("".join(map(str, stack)))
