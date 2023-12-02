ef3bca79bf7f702845f99d40cf44278413afd2bd
sion <shtldhs123@naver.com>
2023-12-02 23:35:21 +0900

    쇠막대기 - if문 코드 정리

--- 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/쇠막대기.py
+++ 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/쇠막대기.py
@@ -4,14 +4,12 @@ res = [0] * len(brackets)
 stack = []  # 여는 괄호, 닫는 괄호
 for i, x in enumerate(brackets):
     if stack and x == ')' and stack[-1][1] == '(':
-        if i == stack[-1][0] + 1:  # 바로 전 인덱스이면, 레이저
-            stack.pop()
-            for tmp in stack:  # 잘린 막대기 cnt
+        top = stack.pop()
+        if i == top[0] + 1:
+            for tmp in stack:
                 res[tmp[0]] += 1
-        else:
-            stack.pop()
-    else:
-        stack.append((i, x))
+        continue
+    stack.append((i, x))
 
 answer = sum([x + 1 for x in res if x > 0])
 print(answer)
