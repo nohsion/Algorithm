02ab723d40542586934321e2b7ec55d8993176a6
sion <shtldhs123@naver.com>
2023-12-02 23:56:35 +0900

    쇠막대기 - 모범 풀이

--- 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/쇠막대기.py
+++ 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/쇠막대기.py
@@ -1,15 +1,31 @@
 brackets = list(map(str, input()))
 
-res = [0] * len(brackets)
-stack = []  # 여는 괄호, 닫는 괄호
-for i, x in enumerate(brackets):
-    if stack and x == ')' and stack[-1][1] == '(':
-        top = stack.pop()
-        if i == top[0] + 1:
-            for tmp in stack:
-                res[tmp[0]] += 1
-        continue
-    stack.append((i, x))
+# 내 풀이
+# res = [0] * len(brackets)
+# stack = []  # 여는 괄호, 닫는 괄호
+# for i, x in enumerate(brackets):
+#     if stack and x == ')' and stack[-1][1] == '(':
+#         top = stack.pop()
+#         if i == top[0] + 1:
+#             for tmp in stack:
+#                 res[tmp[0]] += 1
+#         continue
+#     stack.append((i, x))
+#
+# answer = sum([x + 1 for x in res if x > 0])
+# print(answer)
 
-answer = sum([x + 1 for x in res if x > 0])
-print(answer)
+
+# 모범 답안
+stack = []
+cnt = 0
+for i in range(len(brackets)):
+    if brackets[i] == '(':
+        stack.append(brackets[i])
+    else:
+        stack.pop()
+        if brackets[i-1] == '(':
+            cnt += len(stack)
+        else:
+            cnt += 1
+print(cnt)
