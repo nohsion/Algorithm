3ff1b74e0f6e4f2c3ae82ca0ff3b56b255b70178
sion <shtldhs123@naver.com>
2023-12-10 00:52:39 +0900

    단어 찾기 (딕셔너리 사용)

--- 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/단어 찾기.py
+++ 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/단어 찾기.py
@@ -1,13 +1,11 @@
 n = int(input())
-words = sorted([input() for _ in range(n)])
-a = sorted([input() for _ in range(n-1)])
+words: dict = {}
+for _ in range(n):
+    words[input()] = 1
+for _ in range(n-1):
+    words[input()] -= 1
 
-while words:
-    if not a:
-        print(words.pop())
-        break
-    a_ = a.pop()
-    w_ = words.pop()
-    if a_ != w_:
-        print(w_)
+for k in words.keys():
+    if words[k] == 1:
+        print(k)
         break
