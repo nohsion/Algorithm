6072c73db8b3cf9e90c67f57498335b13861f38a
nohsion <shtldhs123@naver.com>
2023-12-12 17:58:22 +0900

    아나그램 (딕셔너리 풀이 - defaultdict)

--- 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/Anagram.py
+++ 파이썬 알고리즘 문제풀이/4. 자료구조(스택, 큐, 해쉬, 힙)/Anagram.py
@@ -1,5 +1,7 @@
 # Anagram 알파벳의 나열 순서는 다르지만 그 구성이 일치하면 두 단어는 아나그램이라고 합니다.
 # 두 단어가 아나그램인지 판별하세요. (대소문자 구분)
+from collections import defaultdict
+
 a = input()
 b = input()
 
@@ -7,12 +9,16 @@ b = input()
 def is_anagram(a: str, b: str) -> bool:
     if len(a) != len(b):
         return False
-    s1 = sorted(list(map(str, a)))
-    s2 = sorted(list(map(str, b)))
+    a_dict = defaultdict(int)
+    b_dict = defaultdict(int)
     for i in range(len(a)):
-        if s1[i] != s2[i]:
-            return False
-    return True
+        a_dict[a[i]] += 1
+        b_dict[b[i]] += 1
+    if a_dict == b_dict:
+        return True
+    else:
+        return False
+
 
 
 result = is_anagram(a, b)
