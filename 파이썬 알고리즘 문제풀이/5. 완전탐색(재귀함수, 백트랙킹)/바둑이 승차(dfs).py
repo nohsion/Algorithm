4d6e23a4f00b4079df7aa826a7174ef12be28fb9
sion <shtldhs123@naver.com>
2023-12-15 00:26:55 +0900

    바둑이 승차 (DFS, Cut Edge)

--- 파이썬 알고리즘 문제풀이/5. 완전탐색(재귀함수, 백트랙킹)/바둑이 승차(dfs).py
+++ 파이썬 알고리즘 문제풀이/5. 완전탐색(재귀함수, 백트랙킹)/바둑이 승차(dfs).py
@@ -8,21 +8,25 @@
 """
 
 
-def dfs(i, x_sum):
+def dfs(i, x_sum, t_sum):
     global res
     if x_sum > c:
+        return
+    # 중요: (수학적 계산) 현재 합계에 밑에 더할 애들을 더해봐도 현재 최대 합보다 작더라. 그러면 뭐하러 더하냐. cut! (Cut Edge)
+    if x_sum + (total - t_sum) < res:
         return
     if i == n:
         if x_sum > res:
             res = x_sum
         return
-    dfs(i+1, x_sum + weight[i])
-    dfs(i+1, x_sum)
+    dfs(i+1, x_sum + weight[i], t_sum + weight[i])
+    dfs(i+1, x_sum, t_sum + weight[i])
 
 
 if __name__ == '__main__':
     c, n = map(int, input().split())
     weight = [int(input()) for _ in range(n)]
     res = -1
-    dfs(0, 0)
+    total = sum(weight)
+    dfs(0, 0, 0)
     print(res)
