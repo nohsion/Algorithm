cc980c8db0993c7678e330da33ac19e5a35ee7e0
sion <shtldhs123@naver.com>
2023-12-14 23:47:36 +0900

    합이 같은 부분집합 (dfs)

---
+++ 파이썬 알고리즘 문제풀이/5. 완전탐색(재귀함수, 백트랙킹)/합이 같은 부분집합.py
@@ -0,0 +1,35 @@
+# N개의 원소로 구성된 자연수 집합이 주어지면, 이 집합을 두 개의 부분집합으로 나누었을 때
+# 두 부분집합의 원소의 합이 서로 같은 경우가 존재하면 YES, 아니면 NO를 출력하세요.
+"""
+[1, 3, 5, 6, 7, 10]
+       1
+   3       3
+ 5   5   5   5
+6 6 6 6 6 6 6 6
+
+          D(0,0)
+   D(1,1)        D(1,0)
+D(2,4) D(2,1) D(2,3) D(2,0)
+...
+"""
+import sys
+
+
+def dfs(L, x_sum):
+    if x_sum > total // 2:
+        return
+    if L == n:
+        if x_sum == total - x_sum:
+            print("YES")
+            sys.exit(0)
+        return
+    dfs(L + 1, x_sum + a[L])  # 왼쪽 자식 노드 (사용 O)
+    dfs(L + 1, x_sum)  # 오른쪽 자식 노드 (사용 X)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    a = list(map(int, input().split()))
+    total = sum(a)
+    dfs(0, 0)
+    print("NO")
