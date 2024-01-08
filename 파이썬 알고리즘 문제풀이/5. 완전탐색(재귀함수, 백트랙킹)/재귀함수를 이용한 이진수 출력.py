a9892c5020154db70793d96cdaedff483b2f5196
sion <shtldhs123@naver.com>
2024-01-09 00:06:40 +0900

    재귀함수 이진수 dfs (복습)

--- 파이썬 알고리즘 문제풀이/5. 완전탐색(재귀함수, 백트랙킹)/재귀함수를 이용한 이진수 출력.py
+++ 파이썬 알고리즘 문제풀이/5. 완전탐색(재귀함수, 백트랙킹)/재귀함수를 이용한 이진수 출력.py
@@ -1,14 +1,18 @@
 # 10진수 N이 입력되면 2진수로 변환하여 출력하세요.
 # 단, 재귀함수를 사용하여야 합니다.
-# n = int(input())
 
 
 def dfs(x: int):
     if x == 0:
         return
-    dfs(x//2)
-    print(x % 2, end='')  # 중요: 출력 순서를 거꾸로 하려면 dfs 함수 다음에 호출하도록 하면 됨
+    # res.append(x % 2)
+    dfs(x // 2)
+    res.append(x % 2)
 
 
 if __name__ == '__main__':
-    dfs(11)
+    n = int(input())
+    res = []
+    dfs(n)
+    for a in res:
+        print(a, end='')
