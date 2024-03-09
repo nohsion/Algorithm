c28e99a153eaa0f2b8f14492468eb857e1ec45f6
nohsion <shtldhs123@naver.com>
2024-03-09 16:16:03 +0900

    괄호 (백준 9012) - 스택 대신 int로 풀어보기

--- milla_study/stack_queue/baek_9012.py
+++ milla_study/stack_queue/baek_9012.py
@@ -3,18 +3,18 @@
 n = int(input())
 for _ in range(n):
     ps = [x for x in input()]  # (())())
-    stack = []
+    x = 0
     ans = ''
     for a in ps:
         if a == '(':
-            stack.append(a)
+            x += 1
         else:  # ')'
-            if stack:
-                stack.pop()
+            if x > 0:
+                x -= 1
             else:
                 ans = 'NO'
                 break
     if not ans:
-        ans = 'NO' if stack else 'YES'
+        ans = 'NO' if x > 0 else 'YES'
     print(ans)
 
