51726e9af5e55b0641c10f84786f593a562accfb
nohsion <shtldhs123@naver.com>
2024-03-09 17:00:50 +0900

    문자열 뒤집기 (백준 9093) - 버블소트처럼 풀어보기

--- milla_study/stack_queue/baek_9093.py
+++ milla_study/stack_queue/baek_9093.py
@@ -4,4 +4,8 @@ n = int(input())
 for _ in range(n):
     words = input().split()
     for w in words:
-        print(w[::-1], end=' ')
+        w = list(w)
+        for i in range(len(w)-1):
+            for j in range(i+1, len(w)):
+                w[i], w[j] = w[j], w[i]
+        print("".join(w), end=' ')
