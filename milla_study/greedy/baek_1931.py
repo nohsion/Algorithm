cc7a9af5acc498aec37a0148368b658cdc90ea55
nohsion <shtldhs123@naver.com>
2024-03-27 23:11:07 +0900

    회의실 배정 (백준 1931) - 종료시각 정렬해도 오답

--- milla_study/greedy/baek_1931.py
+++ milla_study/greedy/baek_1931.py
@@ -3,7 +3,7 @@
 n = int(input())
 meetings = [tuple(map(int, input().split())) for _ in range(n)]
 cnt, last_end = 0, 0
-for m in sorted(meetings):
+for m in sorted(meetings, key=lambda x: x[1]):
     if m[0] >= last_end:  # 이전 회의가 끝난 시간 이후에 시작하는 미팅만 카운트하면 된다.
         last_end = m[1]
         cnt += 1
