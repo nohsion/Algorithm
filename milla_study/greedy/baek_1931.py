4f5e3b9fc5f3ed6a52b1d1a3886c8647bab251a3
nohsion <shtldhs123@naver.com>
2024-03-27 23:10:52 +0900

    회의실 배정 (백준 1931) - 오답

---
+++ milla_study/greedy/baek_1931.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/1931
+
+n = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(n)]
+cnt, last_end = 0, 0
+for m in sorted(meetings):
+    if m[0] >= last_end:  # 이전 회의가 끝난 시간 이후에 시작하는 미팅만 카운트하면 된다.
+        last_end = m[1]
+        cnt += 1
+print(cnt)
