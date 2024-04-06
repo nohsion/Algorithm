a0449ede4f8b03b199f1cffe1f5dc87ea258c99f
nohsion <shtldhs123@naver.com>
2024-04-06 17:11:11 +0900

    단지번호 붙이기 (백준 2667) - BFS

--- milla_study/dfs_bfs/baek_2667.py
+++ milla_study/dfs_bfs/baek_2667.py
@@ -1,18 +1,24 @@
 # https://www.acmicpc.net/problem/2667
+from collections import deque
 
 
-def dfs(x, y):
+def bfs(x, y):
     global cnt
-    if x < 0 or y < 0 or x > n-1 or y > n-1:
-        return
-
-    if graph[x][y] == 1:
-        cnt += 1
-        graph[x][y] = 0
+    # 첫번째 노드도 카운트 및 방문표시 해야 한다.
+    queue = deque([(x, y)])
+    cnt += 1
+    graph[x][y] = 0
+    while queue:
+        (qx, qy) = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny)
+            nx = qx + dx[i]
+            ny = qy + dy[i]
+            if nx < 0 or ny < 0 or nx > n-1 or ny > n-1:
+                continue
+            if graph[nx][ny] == 1:
+                cnt += 1
+                queue.append((nx, ny))
+                graph[nx][ny] = 0
 
 
 if __name__ == '__main__':
@@ -30,7 +36,7 @@ if __name__ == '__main__':
     for i in range(n):
         for j in range(n):
             if graph[i][j] == 1:
-                dfs(i, j)
+                bfs(i, j)
                 home_cnt.append(cnt)
                 cnt = 0
 
