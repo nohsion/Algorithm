129267a7d91b45f1a92e459c8975a8468a078deb
nohsion <shtldhs123@naver.com>
2024-09-07 18:08:37 +0900

    SWEA 2819 격자판숫자이어붙이기 (int) - int가 str보다 훨씬 빠름

--- SWEA/2819_격자판숫자이어붙이기.py
+++ SWEA/2819_격자판숫자이어붙이기.py
@@ -1,12 +1,12 @@
-def dfs(ci, cj, concat: str):
-    if len(concat) == 7:
-        ans.add(concat)
+def dfs(ci, cj, num: int, cnt: int):
+    if cnt == 7:
+        ans.add(num)
         return
     # 네방향, 범위내
     for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
         ni, nj = ci+di, cj+dj
         if 0<=ni<N and 0<=nj<N:
-            dfs(ni, nj, concat + str(arr[ni][nj]))
+            dfs(ni, nj, num*10 + arr[ni][nj], cnt+1)
 
 T = int(input())
 for test_case in range(1, T + 1):
@@ -15,6 +15,6 @@ for test_case in range(1, T + 1):
     ans = set()
     for i in range(N):
         for j in range(N):
-            dfs(i, j, str(arr[i][j]))
+            dfs(i, j, arr[i][j], 1)
 
     print(f"#{test_case} {len(ans)}")
