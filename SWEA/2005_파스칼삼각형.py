27c05d9981fb40a07d9d217e821eab895b4906c0
nohsion <shtldhs123@naver.com>
2024-09-08 16:05:19 +0900

    SWEA 2005 파스칼삼각형 (0 패딩 처리)

--- SWEA/2005_파스칼삼각형.py
+++ SWEA/2005_파스칼삼각형.py
@@ -1,18 +1,16 @@
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
-    arr = [[0]*(i+1) for i in range(N)]
-    arr[0][0] = 1
+    # 앞에 0으로 패딩 붙이기 -> 복잡한 문제이면, 이렇게 처리하는게 조건이 간단해짐
+    arr = [[0]*(N+1) for i in range(N+1)]
+    arr[1][1] = 1
 
-    for i in range(1, N):
-        for j in range(i+1):
-            if j == 0:
-                arr[i][j] = arr[i-1][j]
-            elif j == i:
-                arr[i][j] = arr[i-1][j-1]
-            else:
-                arr[i][j] = arr[i-1][j] + arr[i-1][j-1]
+    for i in range(2, N+1):
+        for j in range(1, i+1):
+            arr[i][j] = arr[i-1][j] + arr[i-1][j-1]
 
     print(f"#{test_case}")
-    for lst in arr:
-        print(*lst)
+    for i in range(1, N+1):
+        for j in range(1, i+1):
+            print(arr[i][j], end=' ')
+        print()
